2a7ab7f853286c03386cae0b2a4acc98a05ed569
Okan Torun <torun.okn@gmail.com>
2022-08-23 16:00:42 +0300

    anasayfa son(çalıştırmadan önce readmede ki yüklemeler yapılmalıdır)

--- Dashboard/src/pages/loginned/Loginned.jsx
+++ Dashboard/src/pages/loginned/Loginned.jsx
@@ -1,43 +1,43 @@
-import React ,{ useState, useEffect} from "react";
-import  Chart  from "react-apexcharts";
+import React, { useState, useEffect } from "react";
+import Chart from "react-apexcharts";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import "./loginned.scss";
-import TradingViewWidget, {Themes} from "react-tradingview-widget";
+import TradingViewWidget, { Themes } from "react-tradingview-widget";
 
 const Loginned = () => {
-  window.ethereum.on('accountsChanged', async () => {
+  window.ethereum.on("accountsChanged", async () => {
     window.setTimeout(function () {
-    window.location.href='/';
-  }, 1000);
+      window.location.href = "/";
+    }, 1000);
   });
-  
+
   return (
     <div className="home">
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
         <div className="container-fluid mb-3">
-                <Chart 
-                type="pie"
-                width={1100}
-                height={350}
-
-                series={[30,30,40]}                
-
-                options={{
-                       noData:{text:"Empty Data"},                        
-                      // colors:["#f90000","#f0f"],
-                      labels:['Gelen Para','Giden Para','Varlık']                     
-
-                 }}
-                >
-                </Chart>
-            </div>
-        <div className="charts">
-          <TradingViewWidget symbol="BINANCE:ETHUSDT" theme={Themes.LIGHT} locale="en" />
+          <Chart
+            type="pie"
+            width={1349}
+            height={450}
+            series={[30, 30, 40]}
+            options={{
+              noData: { text: "Empty Data" },
+              // colors:["#f90000","#f0f"],
+              labels: ["Gelen Para", "Giden Para", "Varlık"],
+            }}
+          ></Chart>
         </div>
-        
+        <div className="charts">
+          <TradingViewWidget
+            width={1349}
+            symbol="BINANCE:ETHUSDT"
+            theme={Themes.LIGHT}
+            locale="en"
+          />
+        </div>
       </div>
     </div>
   );
